97452ccc47a3b2bdc58ca3265e80ea5aa69228c5
Фарух Темиров <alrov777@yandex.ru>
2022-08-16 08:00:25 +0300

    434

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня не получилось!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
